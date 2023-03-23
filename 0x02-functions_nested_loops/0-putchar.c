10c675a68129316ed749a35b2a990ea34c027be2
kawtar <kawtarelqitani@gmail.com>
2023-03-22 20:08:27 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,15 +3,15 @@
 /**
  * main - Entry point 
  *
- * Description: print _putchar using prototype
+ * Description: prints _putchar using prototype
  *
  * Return: Always 0 (success)
 */
 
 int main(void)
 {
-	char str[] = "_putchar
-	int ch:
+	char str[] = "_putchar";
+	int ch;
 
 	for (ch = 0; ch < 8; ch++)
 		_putchar(str[ch]);
